c67f9bee3abf669cd3d3e189726eaf8f2f0d4e2c
Josh Lazar <lazarjosh2@gmail.com>
2024-04-11 12:25:21 -0400

    updated tailwind on risk calculators page

--- client/src/components/RiskCalculators.jsx
+++ client/src/components/RiskCalculators.jsx
@@ -8,34 +8,27 @@ const RiskCalculators = () => {
         <div className='p-8'>
             <h1 className='text-left text-3xl text-ckdGreen font-bold'>Risk Calculators</h1>
             <div className='border border-t-ckdGreen my-10'></div>
-            <div className='text-left space-y-9'>
-                <div className='flex items-center space-x-6'>
-                    <Link to={'/riskCalculators/KidneyFailureRiskEquation'}><p className='text-2xl text-ckdGreen'>Kidney Failure Risk <br /> Equation <br /> (4 variable)</p></Link>
-                    <FaCalculator className='text-ckdGreen' size={27} />
+            <div className='text-left space-y-16'>
+                <div>
+                    <Link to={'/riskCalculators/KidneyFailureRiskEquation'}><p className='text-2xl text-ckdGreen'>Kidney Failure Risk Equation <br /> (4 variable)</p></Link>
                 </div>
-                <div className='flex items-center space-x-6'>
-                    <Link to={'/riskCalculators/DialysisRiskAfterCardiacSurgeryMehta'}><p className='text-2xl text-ckdGreen'>Dialysis Risk After <br /> Cardiac Surgery <br /> (Mehta)</p></Link>
-                    <FaCalculator className='text-ckdGreen' size={27} />
+                <div>
+                    <Link to={'/riskCalculators/DialysisRiskAfterCardiacSurgeryMehta'}><p className='text-2xl text-ckdGreen'>Dialysis Risk After Cardiac Surgery (Mehta)</p></Link>
                 </div>
-                <div className='flex items-center space-x-6'>
-                    <Link to={'/riskCalculators/DialysisRiskAfterCardiacSurgeryClevelandClinic'}><p className='text-2xl text-ckdGreen'>Dialysis Risk After <br /> Cardiac Surgery <br /> (Cleveland Clinic Score by Thakar)</p></Link>
-                    <FaCalculator className='text-ckdGreen' size={27} />
+                <div>
+                    <Link to={'/riskCalculators/DialysisRiskAfterCardiacSurgeryClevelandClinic'}><p className='text-2xl text-ckdGreen'>Dialysis Risk After Cardiac Surgery <br /> (Cleveland Clinic Score by Thakar)</p></Link>
                 </div>
-                <div className='flex items-center space-x-6'>
-                    <Link to={'/riskCalculators/ContrastNephropathyPost-PCI'}><p className='text-2xl text-ckdGreen'>Contrast Nephropathy <br /> Post-PCI</p></Link>
-                    <FaCalculator className='text-ckdGreen' size={27} />
+                <div>
+                    <Link to={'/riskCalculators/ContrastNephropathyPost-PCI'}><p className='text-2xl text-ckdGreen'>Contrast Nephropathy Post-PCI</p></Link>
                 </div>
-                <div className='flex items-center space-x-6'>
-                    <Link to={'/riskCalculators/ClinicalOutcomesInCKDwithSeverelyDecreasedGFR'}><p className='text-2xl text-ckdGreen'>Clinical outcomes in CKD <br /> with severely decreased GFR</p></Link>
-                    <FaCalculator className='text-ckdGreen' size={27} />
+                <div>
+                    <Link to={'/riskCalculators/ClinicalOutcomesInCKDwithSeverelyDecreasedGFR'}><p className='text-2xl text-ckdGreen'>Clinical outcomes in CKD with severely decreased GFR</p></Link>
                 </div>
-                <div className='flex items-center space-x-6'>
+                <div>
                     <Link to={'/riskCalculators/Apache'}><p className='text-2xl text-ckdGreen'>APACHE II</p></Link>
-                    <FaCalculator className='text-ckdGreen' size={27} />
                 </div>
-                <div className='flex items-center space-x-6'>
-                    <Link to={'/riskCalculators/AkiRiskAfterSurgery'}><p className='text-2xl text-ckdGreen'>AKI risk after surgery <br /> (Postop-MAKE by Woo)</p></Link>
-                    <FaCalculator className='text-ckdGreen' size={27} />
+                <div>
+                    <Link to={'/riskCalculators/AkiRiskAfterSurgery'}><p className='text-2xl text-ckdGreen'>AKI risk after surgery<br />(Postop-MAKE by Woo)</p></Link>
                 </div>
             </div>
         </div>
