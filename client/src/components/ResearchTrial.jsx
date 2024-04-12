1342a08dac2245522e5906801eb3d79b3994dd06
Josh Lazar <lazarjosh2@gmail.com>
2024-04-12 14:02:30 -0400

    switched back to iframe

--- client/src/components/ResearchTrial.jsx
+++ client/src/components/ResearchTrial.jsx
@@ -12,13 +12,12 @@ const ResearchTrial = () => {
             <h1 className='text-left text-3xl text-ckdGreen font-bold'>Topic {topicNumber}</h1>
             <div className='border border-t-ckdGreen my-10'></div>
             <div className='flex justify-center'>
-                {/* <iframe
+                <iframe
                     title={`Topic${topicNumber}`}
-                    className='w-[100%] md:w-[90%] h-[600px] md:h-[800px] '
+                    className='w-[100%] md:w-[90%] h-[600px] md:h-[800px]'
                     scrolling='yes'
                     src={path}
-                ></iframe> */}
-                <embed className='w-[100%] md:w-[90%] h-[600px] md:h-[800px]' src={path} />
+                ></iframe>
             </div>
         </div>
     );
