054e88853f199182407ed546b0fdbb5bc155b28e
Josh Lazar <lazarjosh2@gmail.com>
2024-04-15 11:39:59 -0400

    styling pdfs

--- client/src/components/ResearchTrial.jsx
+++ client/src/components/ResearchTrial.jsx
@@ -20,7 +20,7 @@ const ResearchTrial = () => {
             <div className='flex justify-center'>
                 <iframe
                     title={`Topic${topicNumber}`}
-                    className='w-[100%] h-[800px]'
+                    className='w-[90%] h-[800px]'
                     scrolling='yes'
                     src={paths[topicNumber]}
                     frameBorder='0'
