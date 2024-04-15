54ecbdf473e4972432e8538c8265bd6ee085acf6
Josh Lazar <lazarjosh2@gmail.com>
2024-04-15 11:33:47 -0400

    switched to google drive and its functioning well

--- client/src/components/ResearchTrial.jsx
+++ client/src/components/ResearchTrial.jsx
@@ -8,6 +8,11 @@ const ResearchTrial = () => {
     const path = `/assets/Research/Topic${topicNumber}.pdf`
     const path2 = 'https://drive.google.com/file/d/1pgc-EorpMfbXKThWqQdqDAT9UBL_jDLX/preview'
 
+    const paths = {
+        1: 'https://drive.google.com/file/d/1pgc-EorpMfbXKThWqQdqDAT9UBL_jDLX/preview',
+        2: 'https://drive.google.com/file/d/1WmOiLAMGKXyl1MpzQFIrNu3OmC1Ec0YG/preview'
+    }
+
     return (
         <div className='p-8'>
             <h1 className='text-left text-3xl text-ckdGreen font-bold'>Topic {topicNumber}</h1>
@@ -17,7 +22,7 @@ const ResearchTrial = () => {
                     title={`Topic${topicNumber}`}
                     className='w-[100%] h-[800px]'
                     scrolling='yes'
-                    src={path2}
+                    src={paths[topicNumber]}
                     frameBorder='0'
                 ></iframe>
             </div>
