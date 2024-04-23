9ea944d9cb6c0327a67e0f68a9a90685da923bf1
Josh Lazar <lazarjosh2@gmail.com>
2024-04-23 17:29:02 -0400

    changed pdf links to coming soon pdf

--- client/src/assets/data/trialData.js
+++ client/src/assets/data/trialData.js
@@ -1,9 +1,9 @@
 export const trials = {
-    "IgA-Nephropathy": 'https://drive.google.com/file/d/1pgc-EorpMfbXKThWqQdqDAT9UBL_jDLX/preview',
-    "Lupus-Nephritis": 'https://drive.google.com/file/d/1WmOiLAMGKXyl1MpzQFIrNu3OmC1Ec0YG/preview',
-    "Hypertension": 'https://drive.google.com/file/d/1WmOiLAMGKXyl1MpzQFIrNu3OmC1Ec0YG/preview',
-    "Genetics": 'https://drive.google.com/file/d/1WmOiLAMGKXyl1MpzQFIrNu3OmC1Ec0YG/preview',
-    "Dialysis": 'https://drive.google.com/file/d/1WmOiLAMGKXyl1MpzQFIrNu3OmC1Ec0YG/preview',
-    "Chronic-Kidney-Disease": 'https://drive.google.com/file/d/1WmOiLAMGKXyl1MpzQFIrNu3OmC1Ec0YG/preview',
-    "ANCA-Vasculitis": 'https://drive.google.com/file/d/1WmOiLAMGKXyl1MpzQFIrNu3OmC1Ec0YG/preview',
+    "IgA-Nephropathy": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
+    "Lupus-Nephritis": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
+    "Hypertension": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
+    "Genetics": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
+    "Dialysis": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
+    "Chronic-Kidney-Disease": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
+    "ANCA-Vasculitis": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
 }
