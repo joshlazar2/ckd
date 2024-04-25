e28ef93de643559f6508409aa96d2835f2b9415f
Josh Lazar <lazarjosh2@gmail.com>
2024-04-25 13:28:52 -0400

    added contact info for research trials

--- client/src/assets/data/trialData.js
+++ client/src/assets/data/trialData.js
@@ -1,9 +1,9 @@
 export const trials = {
-    "IgA-Nephropathy": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
-    "Lupus-Nephritis": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
-    "Hypertension": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
-    "Genetics": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
-    "Dialysis": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
-    "Chronic-Kidney-Disease": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
-    "ANCA-Vasculitis": 'https://drive.google.com/file/d/16qCwTM8UUJdDhvyvBVI43rmIseY99S0n/preview',
+    "IgA-Nephropathy": 'https://drive.google.com/file/d/1p9cbIRoMcyp_IzcJtNiWk4XqK4ONHsg5/preview',
+    "Lupus-Nephritis": 'https://drive.google.com/file/d/1ynHXdBl7XoiArRWR2CI12cPLUMrcy3GP/preview',
+    "Hypertension": 'https://drive.google.com/file/d/1OQhBHgvNKEJqvC9J6PVzQSFDhDotUXNW/preview',
+    "Genetics": 'https://drive.google.com/file/d/1MBetHSTlp2PEAaYbhFFESEhunAvS9jlZ/preview',
+    "Dialysis": 'https://drive.google.com/file/d/1nLQZGlioeRokuzwVWvmYUYyx6VtC-0IS/preview',
+    "Chronic-Kidney-Disease": 'https://drive.google.com/file/d/1IPVCZvDx8EN0zG4BFHRq0169TUcL4xnJ/preview',
+    "ANCA-Vasculitis": 'https://drive.google.com/file/d/1_t7Cdo-49Y0INaBNq8AuiigEeIXNBww2/preview',
 }
--- client/src/components/ResearchTrials.jsx
+++ client/src/components/ResearchTrials.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ResearchTrials = () => {
-    return(
+    return (
         <div className='p-8 md:px-20'>
             <h1 className='text-center md:text-left text-3xl text-ckdGreen font-bold'>Research Trials</h1>
+            <div className='space-y-6 mt-6'>
+                <p className='text-left text-2xl text-ckdGreen font-bold'>Clinical Trials Contact</p>
+                <div className='flex flex-col space-y-2'>
+                    <p className='text-left text-xl text-ckdGreen font-bold'>Phone Number:</p>
+                    <a href="tel:+12168960639"><p className='text-left text-lg'>216-844-5396</p></a>
+                </div>
+                <div className='flex flex-col space-y-2'>
+                    <p className='text-left text-xl text-ckdGreen font-bold'>Email:</p>
+                    <a  className='text-left text-md  md:text-lg' href='mailto:NephrologyHTNResearch@uhhospitals.org' target='_blank' rel='noopener noreferrer'>NephrologyHTNResearch@uhhospitals.org</a>
+                </div>
+            </div>
             <div className='border border-t-ckdGreen my-10'></div>
-            <div className='text-center md:text-left space-y-9'>
+            <div className='text-left space-y-9'>
                 <div>
                     <Link to={'/researchTrial/IgA-Nephropathy'}><p className='text-2xl text-ckdGreen'>IgA Nephropathy</p></Link>
                 </div>
