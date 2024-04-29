0e238c31159dffb433de53b17dc733343c6a8302
Josh Lazar <lazarjosh2@gmail.com>
2024-04-29 10:37:35 -0400

    updates

--- client/src/components/RiskCalculators.jsx
+++ client/src/components/RiskCalculators.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FaCalculator } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 const RiskCalculators = () => {
@@ -10,25 +9,25 @@ const RiskCalculators = () => {
             <div className='border border-t-ckdGreen my-10'></div>
             <div className='text-center md:text-left space-y-16'>
                 <div>
-                    <Link to={'/riskCalculators/KidneyFailureRiskEquation'}><p className='text-2xl text-ckdGreen'>Kidney Failure Risk Equation <br /> (4 variable)</p></Link>
+                    <Link to={'/riskCalculators/KidneyFailureRiskEquation'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Kidney Failure Risk Equation <br /> (4 variable)</p></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/DialysisRiskAfterCardiacSurgeryMehta'}><p className='text-2xl text-ckdGreen'>Dialysis Risk After Cardiac Surgery (Mehta)</p></Link>
+                    <Link to={'/riskCalculators/DialysisRiskAfterCardiacSurgeryMehta'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Dialysis Risk After Cardiac Surgery (Mehta)</p></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/DialysisRiskAfterCardiacSurgeryClevelandClinic'}><p className='text-2xl text-ckdGreen'>Dialysis Risk After Cardiac Surgery <br /> (Cleveland Clinic Score by Thakar)</p></Link>
+                    <Link to={'/riskCalculators/DialysisRiskAfterCardiacSurgeryClevelandClinic'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Dialysis Risk After Cardiac Surgery <br /> (Cleveland Clinic Score by Thakar)</p></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/ContrastNephropathyPost-PCI'}><p className='text-2xl text-ckdGreen'>Contrast Nephropathy Post-PCI</p></Link>
+                    <Link to={'/riskCalculators/ContrastNephropathyPost-PCI'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Contrast Nephropathy Post-PCI</p></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/ClinicalOutcomesInCKDwithSeverelyDecreasedGFR'}><p className='text-2xl text-ckdGreen'>Clinical outcomes in CKD with severely decreased GFR</p></Link>
+                    <Link to={'/riskCalculators/ClinicalOutcomesInCKDwithSeverelyDecreasedGFR'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Clinical outcomes in CKD with severely decreased GFR</p></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/Apache'}><p className='text-2xl text-ckdGreen'>APACHE II</p></Link>
+                    <Link to={'/riskCalculators/Apache'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>APACHE II</p></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/AkiRiskAfterSurgery'}><p className='text-2xl text-ckdGreen'>AKI risk after surgery<br />(Postop-MAKE by Woo)</p></Link>
+                    <Link to={'/riskCalculators/AkiRiskAfterSurgery'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>AKI risk after surgery<br />(Postop-MAKE by Woo)</p></Link>
                 </div>
             </div>
         </div>
