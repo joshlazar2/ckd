5dea875834cb92354f1860fcfe64ca41e09398c5
Josh Lazar <lazarjosh2@gmail.com>
2024-05-21 18:34:46 -0400

    updated layout on individual research trials and risk caclulators components

--- client/src/components/ResearchTrial.jsx
+++ client/src/components/ResearchTrial.jsx
@@ -14,15 +14,17 @@ const ResearchTrial = () => {
     return (
         <div className='p-8'>
             <h1 className='text-center md:text-left text-3xl text-ckdGreen font-bold'>{formattedTrialName}</h1>
-            <div className='space-y-6 mt-6'>
-                <p className='text-left text-2xl text-ckdGreen font-bold'>Clinical Trials Contact</p>
-                <div className='flex flex-col space-y-2'>
-                    <p className='text-left text-xl text-ckdGreen font-bold'>Phone Number:</p>
-                    <a href="tel:+12168960639"><p className='text-left text-lg hover:text-ckdGreen'>216-844-5396</p></a>
-                </div>
-                <div className='flex flex-col space-y-2'>
-                    <p className='text-left text-xl text-ckdGreen font-bold'>Email:</p>
-                    <a  className='text-left text-md  md:text-lg hover:text-ckdGreen' href='mailto:NephrologyHTNResearch@uhhospitals.org' target='_blank' rel='noopener noreferrer'>NephrologyHTNResearch@uhhospitals.org</a>
+            <div className='space-y-8 mt-6'>
+                <p className='text-left md:text-center text-2xl text-ckdGreen font-bold'>Clinical Trials Contact</p>
+                <div className='space-y-6 md:space-y-0 flex flex-col md:flex-row md:justify-around'>
+                    <div className='flex flex-col space-y-2'>
+                        <p className='text-left text-xl text-ckdGreen font-bold'>Phone Number:</p>
+                        <a href="tel:+12168960639"><p className='text-left text-lg hover:text-ckdGreen'>216-844-5396</p></a>
+                    </div>
+                    <div className='flex flex-col space-y-2'>
+                        <p className='text-left text-xl text-ckdGreen font-bold'>Email:</p>
+                        <a className='text-left text-md  md:text-lg hover:text-ckdGreen' href='mailto:NephrologyHTNResearch@uhhospitals.org' target='_blank' rel='noopener noreferrer'>NephrologyHTNResearch@uhhospitals.org</a>
+                    </div>
                 </div>
             </div>
             <div className='border border-t-ckdGreen my-10'></div>
--- client/src/components/RiskCalculators.jsx
+++ client/src/components/RiskCalculators.jsx
@@ -7,27 +7,27 @@ const RiskCalculators = () => {
         <div className='p-8'>
             <h1 className='text-center md:text-left text-3xl text-ckdGreen font-bold'>Risk Calculators</h1>
             <div className='border border-t-ckdGreen my-10'></div>
-            <div className='text-center md:text-left space-y-16'>
+            <div className='text-center md:grid grid-cols-3 space-y-9 md:space-y-0 gap-9'>
                 <div>
-                    <Link to={'/riskCalculators/Kidney-Failure-Risk-Equation-(4-Variable)'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Kidney Failure Risk Equation <br /> (4 variable)</p></Link>
+                    <Link to={'/riskCalculators/Kidney-Failure-Risk-Equation-(4-Variable)'}><button className='bg-ckdGreen hover:bg-ckdGreenDarker text-2xl rounded-2xl p-3 text-white w-[270px]'>Kidney Failure Risk Equation <br /> (4 variable)</button></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/Dialysis-Risk-After-Cardiac-Surgery-(Mehta)'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Dialysis Risk After Cardiac Surgery (Mehta)</p></Link>
+                    <Link to={'/riskCalculators/Dialysis-Risk-After-Cardiac-Surgery-(Mehta)'}><button className='bg-ckdGreen hover:bg-ckdGreenDarker text-2xl rounded-2xl p-3 text-white w-[270px]'>Dialysis Risk After Cardiac Surgery (Mehta)</button></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/Dialysis-Risk-After-Cardiac-Surgery-(Cleveland-Clinic-Score-by-Thakar)'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Dialysis Risk After Cardiac Surgery <br /> (Cleveland Clinic Score by Thakar)</p></Link>
+                    <Link to={'/riskCalculators/Dialysis-Risk-After-Cardiac-Surgery-(Cleveland-Clinic-Score-by-Thakar)'}><button className='bg-ckdGreen hover:bg-ckdGreenDarker text-2xl rounded-2xl p-3 text-white w-[270px]'>Dialysis Risk After Cardiac Surgery <br /> (Cleveland Clinic Score by Thakar)</button></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/Contrast-Nephropathy-Post-PCI'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Contrast Nephropathy Post-PCI</p></Link>
+                    <Link to={'/riskCalculators/Contrast-Nephropathy-Post-PCI'}><button className='bg-ckdGreen hover:bg-ckdGreenDarker text-2xl rounded-2xl p-3 text-white w-[270px]'>Contrast Nephropathy Post-PCI</button></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/Clinical-outcomes-in-CKD-with-severely-decreased-GFR'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>Clinical outcomes in CKD with severely decreased GFR</p></Link>
+                    <Link to={'/riskCalculators/Clinical-outcomes-in-CKD-with-severely-decreased-GFR'}><button className='bg-ckdGreen hover:bg-ckdGreenDarker text-2xl rounded-2xl p-3 text-white w-[270px]'>Clinical outcomes in CKD with severely decreased GFR</button></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/APACHE-II'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>APACHE II</p></Link>
+                    <Link to={'/riskCalculators/APACHE-II'}><button className='bg-ckdGreen hover:bg-ckdGreenDarker text-2xl rounded-2xl p-3 text-white w-[270px]'>APACHE II</button></Link>
                 </div>
                 <div>
-                    <Link to={'/riskCalculators/AKI-risk-after-surgery-(Postop-MAKE-by-Woo)'}><p className='text-2xl text-ckdGreen hover:text-ckdGreenDarker'>AKI risk after surgery<br />(Postop-MAKE by Woo)</p></Link>
+                    <Link to={'/riskCalculators/AKI-risk-after-surgery-(Postop-MAKE-by-Woo)'}><button className='bg-ckdGreen hover:bg-ckdGreenDarker text-2xl rounded-2xl p-3 text-white w-[270px]'>AKI risk after surgery<br />(Postop-MAKE by Woo)</button></Link>
                 </div>
             </div>
         </div>
